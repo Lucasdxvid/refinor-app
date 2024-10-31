587b9a896f7585a8a52e94a80a0adae1252c097b
lucasdxvid <athl12311@gmail.com>
2024-10-31 17:09:27 -0300

    Inicio de Repositorio

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/style.css"
+
+function Login() {
+  return (
+    <div>
+      <p>
+        Placeholder
+      </p>
+    </div>
+  );
+}
+
+export default Login;
