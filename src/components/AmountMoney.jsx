b1b8966d458303fc2357683cf1e8d09c0922a1a5
lucasdxvid <athl12311@gmail.com>
2024-11-02 17:59:20 -0300

    amount bar cambio de iconos

--- src/components/AmountMoney.jsx
+++ src/components/AmountMoney.jsx
@@ -1,33 +1,54 @@
 import React from "react";
-import RefundAmountIcon from "../assets/icons/amountBar/RefundAmountIcon.svg";
+import totalAmountIcon from "../assets/icons/amountBar/totalAmountIcon.svg";
+import manualTotalAmountIcon from "../assets/icons/amountBar/manualTotalAmountIcon.svg";
+import benefitAmountIcon from "../assets/icons/amountBar/benefitAmountIcon.svg";
+import refundAmountIcon from "../assets/icons/amountBar/refundAmountIcon.svg";
 export const AmountMoney = () => {
   return (
     <div className="flex gap-4">
-      <div className="p-4 flex flex-col items-center max-w-60">
-        <div className="flex items-center gap-3">
-          <img
-            src={RefundAmountIcon}
-            alt="Refund Amount"
-            className="w-10 h-10"
-          />
-          <span className="font-serif text-base font-normal leading-5 text-left text-slate-800">
+      <div className="p-4 flex items-start max-w-60 gap-5">
+        <img src={totalAmountIcon} alt="Refund Amount" className="w-10 h-10" />
+        <div className="flex flex-col items-start">
+          <span className="font-serif text-base font-normal leading-5 text-left text-slate-800 max-w-48">
             Monto total de transacciones
           </span>
+          <span className="font-semibold text-2xl">$700.000</span>
         </div>
-        <span className="font-semibold text-2xl">$700.000</span>
       </div>
-      <div className="p-4 flex flex-col items-center max-w-60">
-        <div className="flex items-center gap-3">
-          <img
-            src={RefundAmountIcon}
-            alt="Refund Amount"
-            className="w-10 h-10"
-          />
-          <span className="font-serif text-base font-normal leading-5 text-left text-slate-800">
-            Monto total de transacciones
+      <div className="p-4 flex items-start max-w-60 gap-5">
+        <img
+          src={manualTotalAmountIcon}
+          alt="Refund Amount"
+          className="w-10 h-10"
+        />
+        <div className="flex flex-col items-start">
+          <span className="font-serif text-base font-normal leading-5 text-left text-slate-800 max-w-48">
+            Monto total cargado manual
           </span>
+          <span className="font-semibold text-2xl">$50.000</span>
         </div>
-        <span className="font-semibold text-2xl">$700.000</span>
+      </div>
+      <div className="p-4 flex items-start max-w-60 gap-5">
+        <img
+          src={benefitAmountIcon}
+          alt="Refund Amount"
+          className="w-10 h-10"
+        />
+        <div className="flex flex-col items-start">
+          <span className="font-serif text-base font-normal leading-5 text-left text-slate-800 max-w-48">
+            Monto total cargado por beneficio
+          </span>
+          <span className="font-semibold text-2xl">$50.000</span>
+        </div>
+      </div>
+      <div className="p-4 flex items-start max-w-60 gap-5">
+        <img src={refundAmountIcon} alt="Refund Amount" className="w-10 h-10" />
+        <div className="flex flex-col items-start">
+          <span className="font-serif text-base font-normal leading-5 text-left text-slate-800 max-w-48">
+            Monto total devolución
+          </span>
+          <span className="font-semibold text-2xl">$50.000</span>
+        </div>
       </div>
     </div>
   );
